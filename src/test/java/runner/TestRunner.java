6afcae446a217d576478d3ad32208d19c7bc5c77
Saad <saad.jabir@gmail.com>
2021-12-15 20:31:53 -0500

    5th time

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
 		features = "classpath:features",//to link to feature files folder
 		glue = "stepDefinitions",// to link to stepDefinitions folder
-		tags = "@smokeTest", // will run one scenario , or multiple scenarios, or entire feature
+		tags = "@Test", // will run one scenario , or multiple scenarios, or entire feature
 		dryRun= false, //checks if there are any missing steps in stepDefinition class for each scenario 
 		monochrome= true,//Will make console to readable 
 		strict = true,     // will mark scenario failed if there are any pending steps 
