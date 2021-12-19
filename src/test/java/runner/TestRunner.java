d632ab06ecba9b7ce250c49d4e95aaae35404e86
Saad <saad.jabir@gmail.com>
2021-12-18 23:10:23 -0500

    update

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "classpath:features",//to link to feature files folder
+		features = "\\src\\test\\resource\\features",//to link to feature files folder
 		glue = "stepDefinitions",// to link to stepDefinitions folder
 		tags = "@Test", // will run one scenario , or multiple scenarios, or entire feature
 		dryRun= false, //checks if there are any missing steps in stepDefinition class for each scenario 
