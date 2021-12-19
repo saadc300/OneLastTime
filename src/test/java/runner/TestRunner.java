7362d5b4277f9a330add8275a979e36cbea27067
Saad <saad.jabir@gmail.com>
2021-12-19 16:50:54 -0500

    NewClass

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,7 +8,7 @@ import utilities.CucumberReportingConfig;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "classpath:features",
+		features = ".\\src\\test\\resource\\features",
 		glue = "stepDefinitions",// to link to stepDefinitions folder
 		tags = "@Test", // will run one scenario , or multiple scenarios, or entire feature
 		dryRun= false, //checks if there are any missing steps in stepDefinition class for each scenario 
